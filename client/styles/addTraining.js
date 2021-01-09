80ef8a2618c0f6b28d8e82ab9bbc67fc85dad4eb
Travis Williams <williams44t@gmail.com>
2021-01-08 23:40:53 -0800

    adds a bit more still to AddTraining; mainly positioning

--- client/styles/addTraining.js
+++ client/styles/addTraining.js
@@ -3,11 +3,16 @@ import theme from './theme.js';
 
 export default {
   Wrap: styled.div`
-    position: absolute;
-    width: 400px;
-    height: 500px;
-    margin: auto;
-    background-color: red;
+  position: absolute;
+  top: 12.5%;
+  left: 12.5%;
+  width: 75%;
+  height: 75%;
+  border: 2px ridge white;
+  border-radius: 3%;
+  box-shadow: 20px 20px 18px 1px #5e4d4d9c;
+  background-color: #30323bfc;
+  color: white;
   `,
 
 }
