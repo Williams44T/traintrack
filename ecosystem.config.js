f79623f5482606c3b50d81d7e469681926d5467d
Travis Williams <williams44t@gmail.com>
2021-01-23 15:08:09 -0800

    sets up PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'traintracks',
+    script: './server/index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-219-167-96.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/williams44t.pem',
+      ref: 'origin/main',
+      repo: 'https://github.com/Williams44T/traintrack.git',
+      path: '/home/ubuntu/traintracks',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
+
+console.log('test');
